1a78185a450e54757eb707caf1c725f4a5cad14e
youxiaocheng <2412347023@qq.com>
2021-10-07 00:26:14 +0800

    这个关于与spring源码相关联的设计模式举例分析

---
+++ Spring/src/com/atguigu/spring/test/Adapter.java
@@ -0,0 +1,16 @@
+package com.atguigu.spring.test;
+
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class Adapter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// DispatcherServlet
+		//DispatcherServlet
+	}
+
+}
+
+
